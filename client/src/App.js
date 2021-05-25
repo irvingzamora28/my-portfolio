f2f88e0d90c5d3fac8f704dcfdceb9ec14ed4a1c
Irving Zamora <irvingzamora28@gmail.com>
2021-05-24 23:38:04 -0600

    Added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
+import Navbar from './components/Navbar';
 import './index.css';
 
 function App() {
-  return (
-    <>
-      <p className="text-gray-500">Portfolio Tailwindcss start</p>
-    </>
-  );
+	return (
+		<>
+			<Navbar />
+			<header className="bg-white shadow">
+				<div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+					<h1 className="text-3xl font-bold text-gray-900">My Portfolio</h1>
+				</div>
+			</header>
+			<main>
+				<div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+					<div className="px-4 py-6 sm:px-0">
+						<div className="border-4 border-dashed p-2 border-gray-200 rounded-lg h-96">
+							Inside
+						</div>
+					</div>
+				</div>
+			</main>
+		</>
+	);
 }
 
 export default App;
