a389b3dd0e3a3551da2a7560ab967f44087a215f
Irving Zamora <irvingzamora28@gmail.com>
2021-05-26 23:21:17 -0600

    Fixed navbar issue on small devices

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,6 @@ import { Popover, Transition } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
 import portfolioIcon from './assets/images/portfolio.svg'
 
-const navigation = [
-	{ name: 'Product', href: '#' },
-	{ name: 'Features', href: '#' },
-	{ name: 'Marketplace', href: '#' },
-	{ name: 'Company', href: '#' },
-]
 
 function App() {
 	return (
@@ -28,99 +22,10 @@ function App() {
 							<polygon points="50,0 100,0 50,100 0,100" />
 						</svg>
 
-						<Popover>
-							{({ open }) => (
-								<>
-									<div className="fixed bg-white top-0 pt-6 px-4 sm:px-6 lg:px-8 w-full">
-										<nav
-											className="relative flex items-center justify-between sm:h-10 lg:justify-start"
-											aria-label="Global"
-										>
-											<div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
-												<div className="flex items-center justify-between w-full md:w-auto">
-													<a href="#">
-														<span className="sr-only">Workflow</span>
-														<img
-															className="h-8 w-auto sm:h-10"
-															src={portfolioIcon}
-															alt="Workflow"
-														/>
-													</a>
-													<div className="-mr-2 flex items-center md:hidden">
-														<Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-															<span className="sr-only">Open main menu</span>
-															<MenuIcon className="h-6 w-6" aria-hidden="true" />
-														</Popover.Button>
-													</div>
-												</div>
-												<div className="hidden font-medium text-gray-500 hover:text-gray-900 p-6 md:flex">Irving Zamora</div>
-											</div>
-											<div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
-												{navigation.map((item) => (
-													<a key={item.name} href={item.href} className="font-medium text-gray-500 hover:text-gray-900">
-														{item.name}
-													</a>
-												))}
-											</div>
-										</nav>
-									</div>
+							<Navbar />
+						
 
-									<Transition
-										show={open}
-										as={Fragment}
-										enter="duration-150 ease-out"
-										enterFrom="opacity-0 scale-95"
-										enterTo="opacity-100 scale-100"
-										leave="duration-100 ease-in"
-										leaveFrom="opacity-100 scale-100"
-										leaveTo="opacity-0 scale-95"
-									>
-										<Popover.Panel
-											focus
-											static
-											className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
-										>
-											<div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-												<div className="px-5 pt-4 flex items-center justify-between">
-													<div>
-														<img
-															className="h-8 w-auto"
-															src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-															alt=""
-														/>
-													</div>
-													<div className="-mr-2">
-														<Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-															<span className="sr-only">Close main menu</span>
-															<XIcon className="h-6 w-6" aria-hidden="true" />
-														</Popover.Button>
-													</div>
-												</div>
-												<div className="px-2 pt-2 pb-3 space-y-1">
-													{navigation.map((item) => (
-														<a
-															key={item.name}
-															href={item.href}
-															className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
-														>
-															{item.name}
-														</a>
-													))}
-												</div>
-												<a
-													href="#"
-													className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
-												>
-													Log in
-                      </a>
-											</div>
-										</Popover.Panel>
-									</Transition>
-								</>
-							)}
-						</Popover>
-
-						<main className="mt-32 mx-auto max-w-7xl px-4 sm:mt-20 sm:px-6 md:mt-16 lg:mt-18 lg:p-10 xl:mt-18">
+						<main className="mt-8 mx-auto max-w-7xl px-4 sm:mt-20 sm:px-6 md:mt-16 lg:mt-18 lg:p-10 xl:mt-18">
 							<div className="sm:text-center lg:text-left">
 								<h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
 									<span className="block xl:inline">Data to enrich your</span>{' '}
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,94 +1,123 @@
 import React, { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Disclosure, Menu, Popover, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 import portfolioIcon from '../assets/images/portfolio.svg'
 
-const navigation = ['Home', 'Projects', 'Blog']
+
+// const navigation = ['Home', 'Projects', 'Blog']
 const profile = ['Your Profile', 'Settings', 'Sign out']
 
 function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
-  }
+  return classes.filter(Boolean).join(' ')
+}
+
+const open = true
+
+const navigation = [
+	{ name: 'Product', href: '#' },
+	{ name: 'Features', href: '#' },
+	{ name: 'Marketplace', href: '#' },
+	{ name: 'Company', href: '#' },
+]
 
 const Navbar = () => {
-    return (
-        <div>
-            <Disclosure as="nav" className="bg-gray-800">
-        {({ open }) => (
-          <>
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-              <div className="flex items-center justify-between h-16">
-                <div className="flex items-center">
-                  <div className="flex-shrink-0">
-                    <img
-                      className="h-8 w-8"
-                      src={portfolioIcon}
-                      alt="Workflow"
-                    />
-                  </div>
-                  <div className="hidden md:block">
-                    <div className="ml-10 flex items-baseline space-x-4">
-                      {navigation.map((item, itemIdx) =>
-                        itemIdx === 0 ? (
-                          <Fragment key={item}>
-                            <a href="#" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
-                              {item}
-                            </a>
-                          </Fragment>
-                        ) : (
-                          <a
-                            key={item}
-                            href="#"
-                            className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                          >
-                            {item}
-                          </a>
-                        )
-                      )}
-                    </div>
-                  </div>
-                </div>
-                
-                <div className="-mr-2 flex md:hidden">
-                  {/* Mobile menu button */}
-                  <Disclosure.Button className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                    <span className="sr-only">Open main menu</span>
-                    {open ? (
-                      <XIcon className="block h-6 w-6" aria-hidden="true" />
-                    ) : (
-                      <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                    )}
-                  </Disclosure.Button>
-                </div>
+  return (
+    <>
+    <Popover>
+    {({ open }) => (
+    <>
+      <div className="relative md:fixed bg-white top-0 pt-6 px-4 sm:px-6 lg:px-8 w-full">
+        <nav
+          className="relative flex items-center justify-between sm:h-10 lg:justify-start"
+          aria-label="Global"
+        >
+          <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+            <div className="flex items-center justify-between w-full md:w-auto">
+              <a href="#">
+                <span className="sr-only">Workflow</span>
+                <img
+                  className="h-8 w-auto sm:h-10"
+                  src={portfolioIcon}
+                  alt="Workflow"
+                />
+              </a>
+              <div className="-mr-2 flex items-center md:hidden">
+                <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <span className="sr-only">Open main menu</span>
+                  <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                </Popover.Button>
               </div>
             </div>
+            <div className="hidden font-medium text-gray-500 hover:text-gray-900 p-6 md:flex">Irving Zamora</div>
+          </div>
+          <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+            {navigation.map((item) => (
+              <a key={item.name} href={item.href} className="font-medium text-gray-500 hover:text-gray-900">
+                {item.name}
+              </a>
+            ))}
+          </div>
+        </nav>
+      </div>
 
-            <Disclosure.Panel className="md:hidden">
-              <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                {navigation.map((item, itemIdx) =>
-                  itemIdx === 0 ? (
-                    <Fragment key={item}>
-                      <a href="#" className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">
-                        {item}
-                      </a>
-                    </Fragment>
-                  ) : (
-                    <a
-                      key={item}
-                      href="#"
-                      className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                    >
-                      {item}
-                    </a>
-                  )
-                )}
+      <Transition
+        show={open}
+        as={Fragment}
+        enter="duration-150 ease-out"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="duration-100 ease-in"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
+      >
+        <Popover.Panel
+          focus
+          static
+          className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+        >
+          <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+            <div className="px-5 pt-4 flex items-center justify-between">
+              <div>
+                <img
+                  className="h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                  alt=""
+                />
               </div>
-            </Disclosure.Panel>
-          </>
-        )}
-      </Disclosure>
-        </div>
-    )
+              <div className="-mr-2">
+                <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <span className="sr-only">Close main menu</span>
+                  <XIcon className="h-6 w-6" aria-hidden="true" />
+                </Popover.Button>
+              </div>
+            </div>
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                >
+                  {item.name}
+                </a>
+              ))}
+            </div>
+            <a
+              href="#"
+              className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
+            >
+              Log in
+                      </a>
+          </div>
+        </Popover.Panel>
+      </Transition>
+    </>
+    )}
+						</Popover>
+
+    </>
+
+  )
 }
 
 export default Navbar
